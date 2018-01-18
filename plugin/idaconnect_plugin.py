5b1a56b98a88678805da2f55f4555059c969baaa
NeatMonster <neatmonster@hotmail.fr>
2018-01-18 22:25:14 +0100

    Renamed menu entries

--- plugin/idaconnect_plugin.py
+++ plugin/idaconnect_plugin.py
@@ -61,71 +61,18 @@ class IDAConnect(idaapi.plugin_t):
         self.network.uninstall()
 
     def _install_ui(self):
-        self._install_download_action()
-        self._install_upload_action()
+        self._install_open_action()
+        self._install_save_action()
 
     def _uninstall_ui(self):
-        self._uninstall_download_action()
-        self._uninstall_upload_action()
+        self._uninstall_open_action()
+        self._uninstall_save_action()
 
-    ACTION_DOWNLOAD = 'idaconnect:download'
-    ACTION_UPLOAD = 'idaconnect:upload'
+    ACTION_OPEN = 'idaconnect:open'
+    ACTION_SAVE = 'idaconnect:save'
 
-    def _install_download_action(self):
-        class DownloadHandler(idaapi.action_handler_t):
-
-            def activate(self, ctx):
-                return 1
-
-            def update(self, ctx):
-                return idaapi.AST_ENABLE_ALWAYS
-
-        icon_path = plugin_resource('download.png')
-        icon_data = str(open(icon_path, 'rb').read())
-        self._icon_id_download = idaapi.load_custom_icon(data=icon_data)
-
-        action_desc = idaapi.action_desc_t(
-            self.ACTION_DOWNLOAD,
-            "~D~ownload from server...",
-            DownloadHandler(),
-            None,
-            "Load a database from the remote server",
-            self._icon_id_download
-        )
-
-        result = idaapi.register_action(action_desc)
-        if not result:
-            raise RuntimeError("Failed to register download action")
-
-        result = idaapi.attach_action_to_menu(
-            'File/Open...',
-            self.ACTION_DOWNLOAD,
-            idaapi.SETMENU_APP
-        )
-        if not result:
-            RuntimeError("Failed to attach download action")
-
-        logger.info("Installed the 'Download from server' menu entry")
-
-    def _uninstall_download_action(self):
-        result = idaapi.detach_action_from_menu(
-            'File/Open...',
-            self.ACTION_DOWNLOAD
-        )
-        if not result:
-            return False
-
-        result = idaapi.unregister_action(self.ACTION_DOWNLOAD)
-        if not result:
-            return False
-
-        idaapi.free_custom_icon(self._icon_id_download)
-        self._icon_id_download = idaapi.BADADDR
-
-        logger.info("Uninstalled the 'Download from server' menu entry")
-
-    def _install_upload_action(self):
-        class UploadHandler(idaapi.action_handler_t):
+    def _install_open_action(self):
+        class OpenActionHandler(idaapi.action_handler_t):
 
             def activate(self, ctx):
                 return 1
@@ -133,49 +80,102 @@ class IDAConnect(idaapi.plugin_t):
             def update(self, ctx):
                 return idaapi.AST_ENABLE_ALWAYS
 
-        icon_path = plugin_resource('upload.png')
+        icon_path = plugin_resource('open.png')
         icon_data = str(open(icon_path, 'rb').read())
-        self._icon_id_upload = idaapi.load_custom_icon(data=icon_data)
+        self._icon_id_open = idaapi.load_custom_icon(data=icon_data)
 
         action_desc = idaapi.action_desc_t(
-            self.ACTION_UPLOAD,
-            "~U~pload to server...",
-            UploadHandler(),
+            self.ACTION_OPEN,
+            "Open from server...",
+            OpenActionHandler(),
             None,
-            "Save a database to the remote server",
-            self._icon_id_upload
+            "Load a database from the server",
+            self._icon_id_open
         )
 
         result = idaapi.register_action(action_desc)
         if not result:
-            raise RuntimeError("Failed to register upload action")
+            raise RuntimeError("Failed to register open action")
 
         result = idaapi.attach_action_to_menu(
-            'File/Save...',
-            self.ACTION_UPLOAD,
+            'File/Open...',
+            self.ACTION_OPEN,
             idaapi.SETMENU_APP
         )
         if not result:
-            RuntimeError("Failed to attach upload action")
+            RuntimeError("Failed to attach open action")
 
-        logger.info("Installed the 'Upload to server' menu entry")
+        logger.info("Installed the 'Open from server' menu entry")
 
-    def _uninstall_upload_action(self):
+    def _uninstall_open_action(self):
         result = idaapi.detach_action_from_menu(
-            'File/Save...',
-            self.ACTION_UPLOAD
+            'File/Open...',
+            self.ACTION_OPEN
         )
         if not result:
             return False
 
-        result = idaapi.unregister_action(self.ACTION_UPLOAD)
+        result = idaapi.unregister_action(self.ACTION_OPEN)
         if not result:
             return False
 
-        idaapi.free_custom_icon(self._icon_id_upload)
-        self._icon_id_upload = idaapi.BADADDR
+        idaapi.free_custom_icon(self._icon_id_open)
+        self._icon_id_open = idaapi.BADADDR
 
-        logger.info("Uninstalled the 'Upload to server' menu entry")
+        logger.info("Uninstalled the 'Open from server' menu entry")
+
+    def _install_save_action(self):
+        class SaveActionHandler(idaapi.action_handler_t):
+
+            def activate(self, ctx):
+                return 1
+
+            def update(self, ctx):
+                return idaapi.AST_ENABLE_ALWAYS
+
+        icon_path = plugin_resource('save.png')
+        icon_data = str(open(icon_path, 'rb').read())
+        self._icon_id_save = idaapi.load_custom_icon(data=icon_data)
+
+        action_desc = idaapi.action_desc_t(
+            self.ACTION_SAVE,
+            "Save to server...",
+            SaveActionHandler(),
+            None,
+            "Save the database to the server",
+            self._icon_id_save
+        )
+
+        result = idaapi.register_action(action_desc)
+        if not result:
+            raise RuntimeError("Failed to register save action")
+
+        result = idaapi.attach_action_to_menu(
+            'File/Save...',
+            self.ACTION_SAVE,
+            idaapi.SETMENU_APP
+        )
+        if not result:
+            RuntimeError("Failed to attach save action")
+
+        logger.info("Installed the 'Save to server' menu entry")
+
+    def _uninstall_save_action(self):
+        result = idaapi.detach_action_from_menu(
+            'File/Save...',
+            self.ACTION_SAVE
+        )
+        if not result:
+            return False
+
+        result = idaapi.unregister_action(self.ACTION_SAVE)
+        if not result:
+            return False
+
+        idaapi.free_custom_icon(self._icon_id_save)
+        self._icon_id_save = idaapi.BADADDR
+
+        logger.info("Uninstalled the 'Save to server' menu entry")
 
     def _print_banner(self):
         params = PLUGIN_NAME, PLUGIN_VERSION, PLUGIN_AUTHORS
