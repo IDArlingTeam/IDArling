34151de0540f894f6f28722ce3f87b138467119e
NyaMisty <gyc990326@gmail.com>
2018-08-22 22:58:20 +0800

    Fix PEP-8 format for events.py

--- idarling/core/events.py
+++ idarling/core/events.py
@@ -304,7 +304,7 @@ class LocalTypesChangedEvent(Event):
                         Event.decode_bytes(t[3]),
                     ))
                 else:
-                    self.local_type.append(None)    
+                    self.local_type.append(None)
 
     def __call__(self):
         local_type = []
@@ -318,21 +318,21 @@ class LocalTypesChangedEvent(Event):
                 ))
             else:
                 local_type.append(None)
-        
+
         def alloc_oridinal(target_ordinal):
             # get_ordinal_qty() will return (current max ordinal + 1)
-            missing_ord = target_ordinal - ida_typeinf.get_ordinal_qty(None) + 1
+            missing_ord = target_ordinal-ida_typeinf.get_ordinal_qty(None) + 1
             if missing_ord > 0:
                 ida_typeinf.alloc_type_ordinals(None, missing_ord)
 
         for t in local_type:
             logger.debug("Processing: %s", str(t))
             alloc_oridinal(t[0])
-            
+
             # Can't change some local types if not delete them first
             # Example: struct aaa{int a;}'
             ida_typeinf.del_numbered_type(None, t[0])
-            
+
             if t is not None:
                 cur_tinfo = ida_typeinf.tinfo_t()
                 cur_tinfo.deserialize(None, t[1], t[2])
