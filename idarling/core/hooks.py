b59718cccaaf65c178633d343acc28cc48e6c7d6
Lays <poca0725@gmail.com>
2020-01-08 15:41:26 +0800

    Sync op offset

--- idarling/core/hooks.py
+++ idarling/core/hooks.py
@@ -196,6 +196,8 @@ class IDBHooks(Hooks, ida_idp.IDB_Hooks):
             op = "bin"
         elif is_flag(ida_bytes.oct_flag()):
             op = "oct"
+        elif is_flag(ida_bytes.offflag()):
+            op = "offset"
         elif is_flag(ida_bytes.enum_flag()):
             op = "enum"
             id, serial = gather_enum_info(ea, n)
